08cc83caa937009db7bbed92d9e3c260c680294b
ArtemYakushkin <zimberger29101985@gmail.com>
2023-12-11 03:26:58 +0200

    finish adaptiv

--- js/modal.js
+++ js/modal.js
@@ -2,11 +2,13 @@
 
 const modal = document.querySelector('.backdrop');
 const btnOpen = document.querySelector('.modal__btn-open');
+const btnOpen2 = document.querySelector('.modal__btn-open-2');
 const btnClose = document.querySelector('.modal__btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
 btnOpen.addEventListener('click', toggleModal);
+btnOpen2.addEventListener('click', toggleModal);
 btnClose.addEventListener('click', toggleModal);
 
 
